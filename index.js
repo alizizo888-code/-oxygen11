130a2b38cca7f61868f60672c15e95f62d1ece19
alizizo888-code <alizizo888@gmail.com>
2026-09-19 05:44:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const fs = require("fs");
-const path = require("path");
+const path = path || require("path");
 
 const PORT = process.env.PORT || 3000;
 const PUBLIC_DIR = path.join(__dirname, "public");
